eedf0fc9aef487dd2601674782ef534b6cd7bbc9
forDecember <1921567337@qq.com>
2024-09-30 00:02:02 +0800

    update: 创建评论

---
+++ src/controller/tree_hole/comments.go
@@ -0,0 +1,57 @@
+package tree_hole
+
+import (
+	"cengkeHelperDev/src/models"
+	"cengkeHelperDev/src/service"
+	"cengkeHelperDev/src/utils/logger"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func CommentsCreateOneHandler(c *gin.Context) {
+
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		logger.Warning(err)
+		c.JSON(http.StatusBadRequest, models.NewBadResp("参数错误！"))
+		return
+	}
+
+	// 从token中获取用户id和用户名
+	authorIdStr := "1"
+	authorName := "jack"
+	authorId, _ := strconv.Atoi(authorIdStr)
+
+	// 从请求中获取评论内容
+	type T struct {
+		Content string `json:"content"`
+	}
+	req := T{}
+	if err := c.ShouldBind(&req); err != nil {
+		logger.Warning(err)
+		c.JSON(http.StatusBadRequest, models.NewBadResp("参数错误！"))
+		return
+	}
+
+	if req.Content == "" {
+		c.JSON(http.StatusBadRequest, models.NewBadResp("评论内容不能为空！"))
+		return
+	}
+
+	// 保存评论
+	if err := service.SaveComment(
+		uint32(authorId), uint32(id),
+		authorName, req.Content); err != nil {
+		logger.Warning(err)
+		c.JSON(http.StatusBadRequest, models.NewBadResp("评论失败！"))
+		return
+	}
+
+	c.JSON(http.StatusOK, models.RespData{
+		Code: 200,
+		Data: nil,
+		Msg:  "success",
+	})
+}
--- src/service/comments.go
+++ src/service/comments.go
@@ -3,6 +3,8 @@ package service
 import (
 	"cengkeHelperDev/src/dbmodels"
 	"cengkeHelperDev/src/models"
+	"cengkeHelperDev/src/storage/database"
+	"gorm.io/gorm"
 )
 
 // 内部使用
@@ -18,4 +20,43 @@ func GetCommentsByPostIdWithPage(
 	pageSize int,
 	postId uint32) *models.DividePageWrapper[dbmodels.CommentRecord] {
 	return getCommentsWithPage(page, pageSize, "post_id = ?", postId)
+}
+
+func SaveComment(authorId, postId uint32, authorName, content string) error {
+
+	if err := database.Client.Transaction(func(tx *gorm.DB) error {
+		// 获取当前帖子的评论数
+		var count int64
+		if err := tx.Model(&dbmodels.PostRecord{}).
+			Where("id = ?", postId).Count(&count).Error; err != nil {
+			return err
+		}
+
+		// 保存评论
+		comment := &dbmodels.CommentRecord{
+			PostId:     postId,
+			AuthorId:   authorId,
+			AuthorName: authorName,
+			Content:    content,
+			FloorNum:   uint32(count) + 1,
+		}
+
+		if err := tx.Save(comment).Error; err != nil {
+			return err
+		}
+
+		// 更新帖子的评论数
+		if err := tx.Model(&dbmodels.PostRecord{}).
+			Where("id = ?", postId).
+			Update("comment_count", count+1).Error; err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+
 }
